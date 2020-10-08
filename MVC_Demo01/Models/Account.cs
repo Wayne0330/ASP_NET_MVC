a77bba7513a9421dbb08bf11ba4464c3d7960d95
Wayne <wei840330110022@gmail.com>
2020-10-09 00:18:49 +0800

    add service

--- MVC_Demo01/Models/Account.cs
+++ MVC_Demo01/Models/Account.cs
@@ -15,6 +15,7 @@ namespace Job_Demo.Models
     public partial class Account
     {
         public int Id { get; set; }
+        public string Identity { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
