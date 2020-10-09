5839c3ccc62eced3da77ca97a67ecbc38d68ec9b
Wayne <wei840330110022@gmail.com>
2020-10-09 11:08:13 +0800

    建立註冊系統

---
+++ MVC_Demo01/Controllers/PortalController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Job_Demo.Models;
+using Job_Demo.Services;
+
+namespace Job_Demo.Controllers
+{
+    public class PortalController : Controller
+    {
+        ProtalDBService data = new ProtalDBService();
+        // GET: Portal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Success() {
+            return View();
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string UserName, string Identity, string Password, string Email) 
+        {
+            data.DBCreate(UserName, Identity, Password, Email);
+            return RedirectToAction("Success");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Password, string Email)
+        {
+            data.AccountLogin(Password, Email);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MVC_Demo01/Services/ProtalDBService.cs
@@ -0,0 +1,37 @@
+﻿using Job_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace Job_Demo.Services
+{
+    public class ProtalDBService
+    {
+        public Job_Demo.Models.Job_DemoDBEntities db = new Models.Job_DemoDBEntities();
+
+        public List<Account> GetAccount() {
+            return (db.Account.ToList());
+        }
+
+        public void DBCreate(string Username, string Password, string Email, string Identity) {
+            Account AccountData = new Account();
+            AccountData.UserName = Username;
+            AccountData.Password = Password;
+            AccountData.Email = Email;
+            AccountData.Identity = Identity;
+            AccountData.Time = DateTime.Now;
+            db.Account.Add(AccountData);
+            db.SaveChanges();
+        }
+
+        public void AccountLogin(string Password, string Email)
+        {
+            Account AccountData = new Account();
+            AccountData.Password = Password;
+            AccountData.Email = Email;
+        }
+
+    }
+}
